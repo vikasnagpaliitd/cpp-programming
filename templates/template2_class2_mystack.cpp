dc7ec2bda47483383d879865d607ef3628905e4f
Vikas Nagpal <vikas.nagpal@gmail.com>
2023-02-12 09:21:21 +0530

    .

--- templates/template2_class2_mystack.cpp
+++ templates/template2_class2_mystack.cpp
@@ -1,4 +1,4 @@
-//My Stack
+//Demonstrates: MyStack: stack implementation using array
 #include <iostream>
 using namespace std;
 
@@ -8,23 +8,23 @@ class MyStack
 	private:
 
 	static const int max_stack_len; // Class variable (not instance variable)
-	T arr[max_stack_len];
-	int top; //Indicates index tilll which stack is filled
+	T arr[max_stack_len]; // notice: dimension must be known at compile time
+	int top; // Indicates index tilll which stack is filled
 
 	public:
 
-	MyStack<T>() 
-//	MyStack() // Ques: should we write MyStack<T>
+	MyStack<T>()
+	// MyStack() // Ques: should we write MyStack<T>
 	{
 		top = -1;
 	}
 	
 
-	//Ques: if we wish to implement is_empty outside class, how to do it?
-	bool is_empty()
-	{
-		return (top == -1);
-	}
+	// Ques: if we wish to implement is_empty outside class, how to do it?
+	bool is_empty();
+	// {
+	// 	return (top == -1);
+	// }
 
 	bool is_full()
 	{
@@ -45,9 +45,14 @@ class MyStack
 			throw string("exception:pop in empty stack"); // Normally exception object would be thrown
 
 		return arr[top--];
-
 	}
 };
+
+template <class T>
+bool MyStack<T>::is_empty()
+{
+	return (top == -1);
+}
 
 template <class T>
 const int MyStack<T>::max_stack_len = 3; // Small value so that stack actually can get full
@@ -57,14 +62,12 @@ int main()
 {
 	try
 	{
-	 	//MyStack<string> s_stack(); // Ques: will not work. why?
 	 	MyStack<string> s_stack;
 	 
 	 	s_stack.push("one");
 	 	s_stack.push("two");
 	 	s_stack.push("three");
 	 	//s_stack.push("four"); // will raise exception as stack full
-	 
 	 
 	 	MyStack<int> i_stack;
 	 	i_stack.push(10);
